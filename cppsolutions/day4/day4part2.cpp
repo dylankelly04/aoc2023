d0049c853088f440bfbca349bf4894af0e5041da
dylan kelly <dkelly66@gatech.edu>
2023-12-18 11:42:01 -0500

    Solved Day 4 Part 2

--- cppsolutions/day4/day4part2.cpp
+++ cppsolutions/day4/day4part2.cpp
@@ -6,10 +6,16 @@ int main()
 {
     int total = 0;
     string temp;
+    int multipliers[197];
+    for (int i = 0; i < 197; i++)
+    {
+        multipliers[i] = 0;
+    }
     cin >> temp;
     for (int i = 0; i < 197; i++)
     {
         int count = 0;
+        multipliers[i]++;
         cin >> temp;
         set<string> set;
         cin >> temp;
@@ -23,18 +29,22 @@ int main()
             cin >> temp;
             if (set.find(temp) != set.end())
             {
-                if (count == 0)
-                {
-                    count = 1;
-                }
-                else
-                {
-                    count *= 2;
-                }
+                count++;
             }
         }
-        total += count;
+        for (int j = 1; j <= count; j++)
+        {
+            if (i + j >= 197)
+            {
+                break;
+            }
+            multipliers[i + j] += multipliers[i];
+        }
         count = 0;
+    }
+    for (int i = 0; i < 197; i++)
+    {
+        total += multipliers[i];
     }
     cout << total << endl;
     return 0;
